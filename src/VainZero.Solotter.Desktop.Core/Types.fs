bab525c70118a37c6642bbf2b6127d9e2a3aab05
vain0x <vainzerox@gmail.com>
2017-10-24 22:44:43 +0900

    Indent

--- src/VainZero.Solotter.Desktop.Core/Types.fs
+++ src/VainZero.Solotter.Desktop.Core/Types.fs
@@ -1,17 +1,17 @@
 ﻿namespace VainZero.Solotter.Desktop
 
-open System
-open VainZero.Solotter
+  open System
+  open VainZero.Solotter
 
-type AuthState =
-  /// Indicates we have neither app/user access token.
-  | AppAuth
-  /// Indicates we have an app access token but no user one.
-  | UserAuth
-    of AppAccessToken
-  | CompleteAuth
-    of AppAccessToken * UserAccessToken
+  type AuthState =
+    /// Indicates we have neither app/user access token.
+    | AppAuth
+    /// Indicates we have an app access token but no user one.
+    | UserAuth
+      of AppAccessToken
+    | CompleteAuth
+      of AppAccessToken * UserAccessToken
 
-type IAuthPage =
-  inherit IObservable<AuthState>
-  inherit IDisposable
+  type IAuthPage =
+    inherit IObservable<AuthState>
+    inherit IDisposable
